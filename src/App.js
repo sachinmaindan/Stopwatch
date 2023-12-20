4160409314d199483de81b2f113c465aa26fba68
sachinmaindan <sachinmaindan12@gmail.com>
2023-12-20 16:34:35 +0530

    commit done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
+var timer = 0;
 function App() {
+
+  const [start, setStart]= useState(false);
+  const [stop, setStop]= useState(false);
+  const [sec, setSec]= useState(0);
+  const [timerString,setTimerString]=useState('')
+
+  useEffect(()=>{
+    console.log('running useEffect')
+    const date = new Date(0)
+    date.setSeconds(sec)
+    setTimerString(date.toISOString().slice(11,19))
+  })
+
+  useEffect(()=>{
+    // let timer= null
+    console.log("state values", start, stop)
+    if(start&!stop){
+      timer= setInterval(()=>{
+        setSec(sec=>sec+1)},1000)
+
+      }
+      else if(stop){
+        clearInterval(timer)
+      }
+      // return ()=>(
+      //   clearInterval(timer)
+      // )
+    },[start,stop])
+
+
+    const handleStart=()=>
+    {
+      console.log("handle start running")
+      setStop(false)
+      setStart(true)
+    }
+
+  const handleStop=()=>{
+    console.log("handle stop running")
+    setStart(false)
+    setStop(true)
+  }
+    
+  const handleReset=()=>{
+    console.log("handle reset running")
+    setSec(0);
+    setStart(false);
+    setStop(false);
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+      <p>{sec}</p>
+      <p>{timerString}</p>
+      <div className="button">
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+      </div>
     </div>
   );
 }
